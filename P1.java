8b072c8c1b386761497bcb61950afa2ac2b9b8ec
HABEEB <habeeb.study@gmail.com>
2024-02-03 22:13:37 +0530

    new html added

--- P1.java
+++ P1.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class P1 {
+public class P1 {
+    int num;
+    String name;
+
+    public P1(int num, String name) {
+        this.num = num;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        P1 habeeb = new P1(9966,"Habeeb is a Good boy");
+        System.out.println(habeeb.name);
+        System.out.println(habeeb.num);
+    }
 }
